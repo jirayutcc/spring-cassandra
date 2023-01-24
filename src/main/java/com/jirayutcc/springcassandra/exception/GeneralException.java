433c6fa2d7915d5481963989f1a1da42fae89f35
jirayut cc <jirayut_ssww@hotmail.com>
2023-01-24 14:36:58 +0700

    added GeneralException

---
+++ src/main/java/com/jirayutcc/springcassandra/exception/GeneralException.java
@@ -0,0 +1,9 @@
+package com.jirayutcc.springcassandra.exception;
+
+public class GeneralException extends RuntimeException {
+
+    public GeneralException(String msg) {
+        super(msg);
+    }
+
+}
