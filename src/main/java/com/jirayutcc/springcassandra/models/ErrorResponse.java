57de6c2f23fdb2cebe2769061d79fa201a54db81
jirayut cc <jirayut_ssww@hotmail.com>
2023-01-27 20:04:48 +0700

    added ErrorResponse

---
+++ src/main/java/com/jirayutcc/springcassandra/models/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.jirayutcc.springcassandra.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ErrorResponse {
+
+    private String status;
+    private String message;
+
+}
